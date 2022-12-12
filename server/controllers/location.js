ad9977704ef0704dfdf05aff2378b57c4d9f7a47
Minh.NN <minh.nguyennhat@ddsolution.vn>
2022-12-12 14:39:12 +0700

    implement apis

--- server/controllers/location.js
+++ server/controllers/location.js
@@ -1,1 +1,24 @@
-export const getFreeLocation = async (req, res) => {}
+import { getLocationList } from '@root/repository/locationRepository'
+
+export const getFreeLocation = async (req, res) => {
+  try {
+    const { credit } = req.body
+    const locationList = await getLocationList({})
+    const freeLocation = []
+    const freeTime = []
+    locationList.forEach((location) => {
+      const busyTime = location.locationBusyTime
+      for (let i = 0; i < busyTime.length - 1; i++) {
+        if (busyTime[i + 1] - busyTime[i] > credit) {
+          for (let j = busyTime[i] + 1; j < busyTime[i + 1]; j++) {
+            freeTime.push[j]
+          }
+        }
+      }
+      freeLocation.push({ locationName: location.locationName, freeTime })
+    })
+    return { freeLocation }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
