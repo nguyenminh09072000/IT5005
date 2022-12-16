3a934397ecf661bffa14dc322afe323b0e222056
Minh.NN <minh.nguyennhat@ddsolution.vn>
2022-12-16 14:02:06 +0700

    fix

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://172.17.0.1:27108/IT5005').catch((error) => {
-  process.exit(1)
-})
+mongoose
+  .connect('mongodb+srv://minhnn:minhnn123@cluster0.oqpblan.mongodb.net/?retryWrites=true&w=majority')
+  .catch((error) => {
+    process.exit(1)
+  })
 const mongooseConnection = mongoose.connection
 
 export default mongooseConnection
