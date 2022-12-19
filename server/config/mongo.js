8f7f178a965622a3d709ad8a7e3f6eedb7bef71c
Phạm Văn Nam <nam.pv1805@gmail.com>
2022-12-19 14:10:00 +0700

    Never gonna let you down

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,9 +1,16 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://172.17.0.1:27108/IT5005').catch((error) => {
-  console.log(error)
-  process.exit(1)
-})
-const mongooseConnection = mongoose.connection
+mongoose
+    .connect(
+        'mongodb+srv://minhnn:minhnn123@cluster0.oqpblan.mongodb.net/?retryWrites=true&w=majority'
+    )
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch(error => {
+        console.log(error);
+        process.exit(1);
+    });
+const mongooseConnection = mongoose.connection;
 
-export default mongooseConnection
+export default mongooseConnection;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,30 @@
-import studentRouter from './student'
-import classRouter from '@root/routes/class'
-import subjectRouter from '@root/routes/subject'
-import locationRouter from '@root/routes/location'
-import teacherRouter from '@root/routes/teacher'
+import studentRouter from './student';
+import classRouter from '@root/routes/class';
+import subjectRouter from '@root/routes/subject';
+import locationRouter from '@root/routes/location';
+import teacherRouter from '@root/routes/teacher';
+import swaggerUi from 'swagger-ui-express';
 
-const router = (app) => {
-  // app.use('/admin', adminRouter)
-  app.use('/class', classRouter)
-  app.use('/student', studentRouter)
-  app.use('/subject', subjectRouter)
-  app.use('/teacher', teacherRouter)
-  app.use('/location', locationRouter)
-}
+var options = {
+    explorer: true,
+    openapi: '3.0.0',
+    basePath: '/',
+    swaggerOptions: {
+        validatorUrl: null,
+    },
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+};
 
-export default router
+const router = app => {
+    // app.use('/admin', adminRouter)
+    app.use('/class', classRouter);
+    app.use('/student', studentRouter);
+    app.use('/subject', subjectRouter);
+    app.use('/teacher', teacherRouter);
+    app.use('/location', locationRouter);
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(options));
+};
+
+export default router;
