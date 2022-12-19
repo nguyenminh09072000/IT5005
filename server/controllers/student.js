4cf9cc1b5c660a61db6139886a01a637c6206980
Minh.NN <minh.nguyennhat@ddsolution.vn>
2022-12-19 15:01:30 +0700

    add user role

--- server/controllers/student.js
+++ server/controllers/student.js
@@ -1,79 +1,98 @@
 import {
-  createNewStudent,
-  findAndDeleteStudent,
-  findStudent,
-  findStudentAndUpdate,
-} from '@root/repository/studentRepository'
-import { findClass } from '@root/repository/classRepository'
+    createNewStudent,
+    findAndDeleteStudent,
+    findStudent,
+    findStudentAndUpdate,
+} from '@root/repository/studentRepository';
+import {findClass} from '@root/repository/classRepository';
+import {ROLES} from '@root/utils/constant';
 
 export const getStudent = async (req, res) => {
-  try {
-    const { studentId } = req.query
-    const filter = { studentId }
-    const student = await findStudent(filter)
-    return res.json(student)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {role} = req;
+        if (role === ROLES.TEACHER) {
+            return res.json({message: 'Invalid role'});
+        }
+        const {studentId} = req.query;
+        const filter = {studentId};
+        const student = await findStudent(filter);
+        return res.json(student);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const updateStudent = async (req, res) => {
-  try {
-    const { studentId, studentName } = req.body
-    const student = await findStudentAndUpdate({ studentId }, { studentName }, { new: true })
-    return res.json(student)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {role} = req;
+        if (role === ROLES.TEACHER) {
+            return res.json({message: 'Invalid role'});
+        }
+        const {studentId, studentName} = req.body;
+        const student = await findStudentAndUpdate({studentId}, {studentName}, {new: true});
+        return res.json(student);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const createStudent = async (req, res) => {
-  try {
-    const { studentId, studentName, accountId } = req.body
-    const student = await createNewStudent([{ studentId, studentName, accountId }])
-    return res.json(student)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {role} = req;
+        if (role !== ROLES.ADMIN) {
+            return res.json({message: 'Invalid role'});
+        }
+        const {studentId, studentName, accountId} = req.body;
+        const student = await createNewStudent([{studentId, studentName, accountId}]);
+        return res.json(student);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const deleteStudent = async (req, res) => {
-  try {
-    const { studentId } = req.body
-    await findAndDeleteStudent({ studentId })
-    return res.json({ message: 'Deleted' })
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {studentId} = req.body;
+        await findAndDeleteStudent({studentId});
+        return res.json({message: 'Deleted'});
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const getStudentClassList = async (req, res) => {
-  try {
-    const { studentId } = req.body
-    const student = await findStudent({ studentId })
+    try {
+        const {role} = req;
+        if (role !== ROLES.ADMIN) {
+            return res.json({message: 'Invalid role'});
+        }
+        const {studentId} = req.body;
+        const student = await findStudent({studentId});
 
-    const classIDs = student.classes
-    const classList = []
+        const classIDs = student.classes;
+        const classList = [];
 
-    for (const ele of classIDs) {
-      const classInfo = await findClass({ classId: ele })
+        for (const ele of classIDs) {
+            const classInfo = await findClass({classId: ele});
 
-      if (classInfo.length) {
-        const classStudent = classInfo[0].students
-        const studentInClass = classStudent.filter((element) => element.studentId === studentId)
-        classList.push({
-          classId: ele,
-          subjectId: classInfo[0].subjectId,
-          subjectName: classInfo[0].subjectName,
-          midterm: studentInClass[0].midterm,
-          final: studentInClass[0].final,
-          locationName: classInfo[0].locationName,
-          classBusyTime: classInfo[0].classBusyTime,
-        })
-      }
+            if (classInfo.length) {
+                const classStudent = classInfo[0].students;
+                const studentInClass = classStudent.filter(
+                    element => element.studentId === studentId
+                );
+                classList.push({
+                    classId: ele,
+                    subjectId: classInfo[0].subjectId,
+                    subjectName: classInfo[0].subjectName,
+                    midterm: studentInClass[0].midterm,
+                    final: studentInClass[0].final,
+                    locationName: classInfo[0].locationName,
+                    classBusyTime: classInfo[0].classBusyTime,
+                });
+            }
+        }
+        return res.json(classList);
+    } catch (error) {
+        res.status(500).json(error);
     }
-    return res.json(classList)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+};
--- server/controllers/teacher.js
+++ server/controllers/teacher.js
@@ -1,57 +1,70 @@
 import {
-  createNewTeacher,
-  findAndDeleteTeacher,
-  findTeacher,
-  findTeacherAndUpdate,
-} from '@root/repository/teacherRepository'
-import { findClass } from '@root/repository/classRepository'
+    createNewTeacher,
+    findAndDeleteTeacher,
+    findTeacher,
+    findTeacherAndUpdate,
+} from '@root/repository/teacherRepository';
+import {findClass} from '@root/repository/classRepository';
+import {ROLES} from '@root/utils/constant';
 
 export const getTeacher = async (req, res) => {
-  try {
-    const { teacherId } = req.query
-    const teacher = await findTeacher({ teacherId })
-    return res.json(teacher)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {teacherId} = req.query;
+        const teacher = await findTeacher({teacherId});
+        return res.json(teacher);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const updateTeacher = async (req, res) => {
-  try {
-    const { teacherId, updateInfo } = req.body
-    const teacher = await findTeacherAndUpdate({ teacherId }, updateInfo, { new: true })
-    return res.json(teacher)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {teacherId, updateInfo} = req.body;
+        const {role} = req;
+        if (role === ROLES.STUDENT) {
+            return res.json({message: 'Invalid role'});
+        }
+        const teacher = await findTeacherAndUpdate({teacherId}, updateInfo, {new: true});
+        return res.json(teacher);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const createTeacher = async (req, res) => {
-  try {
-    const { teacherId, teacherName, accountId } = req.body
-    const teacher = await createNewTeacher([{ teacherId, teacherName, accountId }])
-    return res.json(teacher)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {role} = req;
+        if (role !== ROLES.ADMIN) {
+            return res.json({message: 'Invalid role'});
+        }
+        const {teacherId, teacherName, accountId} = req.body;
+        const teacher = await createNewTeacher([{teacherId, teacherName, accountId}]);
+        return res.json(teacher);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const deleteTeacher = async (req, res) => {
-  try {
-    const { teacherId } = req.body
-    await findAndDeleteTeacher({ teacherId })
-    return res.json({ message: 'Deleted' })
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {role} = req;
+        if (role !== ROLES.ADMIN) {
+            return res.json({message: 'Invalid role'});
+        }
+        const {teacherId} = req.body;
+        await findAndDeleteTeacher({teacherId});
+        return res.json({message: 'Deleted'});
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
 
 export const getTeacherClassList = async (req, res) => {
-  try {
-    const { teacherId } = req.body
-    const classList = await findClass({ teacherId })
-    return res.json(classList)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-}
+    try {
+        const {teacherId} = req.body;
+        const classList = await findClass({teacherId});
+        return res.json(classList);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
