a542fc7047abb4b7b716db92397d80eea1dcd1db
congminhdinh <correct-email@example.com>
2023-02-22 03:19:46 +0700

    result

--- client/src/pages/student/Result.js
+++ client/src/pages/student/Result.js
@@ -1,59 +1,32 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import { Container } from '@mui/system';
+import { Container, Snackbar, Alert} from '@mui/material';
 import { useState, useEffect } from 'react';
 import TokenService from '../../service/TokenService';
 import GmailService from '../../service/GmailService';
-const columns = [
-    { field: 'term', headerName: 'Học kỳ', width: 120 },
-    {
-        field: 'id',
-        headerName: 'Mã Học Phần',
-        width: 150,
-    },
-    {
-        field: 'SubName',
-        headerName: 'Tên Học Phần',
-        width: 310,
-    },
-    {
-        field: 'credit',
-        headerName: 'TC',
-        type: 'number',
-        width: 110,
-    },
-    {
-        field: 'Mark',
-        headerName: 'Điểm học phần',
-        width: 150,
-    },
-];
-
-function Result() {
-    const rows = [
-        { term: '20211', id: 'IT4931', SubName: 'Lưu trữ và xử lý dữ liệu lớn', credit: 3, Mark: 'A+' },
-        { term: '20211', id: 'IT4930', SubName: 'Nhập môn Khoa học dữ liệu', credit: 2, Mark: 'B+' },
-        { term: '20211', id: 'IT4906', SubName: 'Tính toán tiến hóa	', credit: 2, Mark: 'A' },
-        { term: '20211', id: 'IT4653', SubName: 'Học sâu và ứng dụng', credit: 2, Mark: 'B' },
-        { term: '20211', id: 'IT4409', SubName: 'Công nghệ Web và dịch vụ trực tuyến', credit: 3, Mark: 'B' },
-        { term: '20211', id: 'IT3940', SubName: 'Project III', credit: 3, Mark: 'A+' },
-        { term: '20202', id: 'SSH1151', SubName: 'Tư tưởng Hồ Chí Minh', credit: 2, Mark: 'A' },
-        { term: '20202', id: 'IT4991', SubName: 'Thực tập kỹ thuật', credit: 2, Mark: 'A+' },
-        { term: '20202', id: 'IT4015', SubName: 'Nhập môn an toàn thông tin', credit: 3, Mark: 'A' },
-        { term: '20202', id: 'IT3930', SubName: 'Project II', credit: 2, Mark: 'A+' },
-        { term: '20202', id: 'IT3190', SubName: 'Nhập môn Học máy và khai phá dữ liệu', credit: 3, Mark: 'A' },
-        { term: '20202', id: 'IT3170', SubName: 'Thuật toán ứng dụng', credit: 2, Mark: 'B+' },
-        { term: '20202', id: 'IT3120', SubName: 'Phân tích thiết kế hệ thống', credit: 2, Mark: 'B+' },
-        { term: '20202', id: 'IT2030', SubName: 'Technical Writing and Presentation', credit: 3, Mark: 'A' },
-        { term: '20202', id: 'ED3220', SubName: 'Kỹ năng mềm', credit: 2, Mark: 'A' },
-    ];
-    const [row, setRow] = useState();
-
-    useEffect(() => {
+class Result extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            columns: [
+                { field: 'id', headerName: 'Index', width: 100 },
+                { field: 'SubId', headerName: 'Mã học phần', width: 150 },
+                { field: 'SubName', headerName: 'Tên học phần', width: 250 },
+                { field: 'credit', headerName: 'TC', width: 100 },
+                { field: 'Midterm', headerName: 'Điểm giữa kỳ', width: 100 },
+                { field: 'Finalterm', headerName: 'Điểm cuối kỳ', width: 100 },
+            ],
+            count: 0,
+            rows: [],
+            open: false,
+            severity: '',
+            dataAPI: [],
+            message: '',
+        }
+    }
+    async componentDidMount() {
         const username = GmailService.getLocalGmail();
-        async function fetchMyAPI() {
-            const username = GmailService.getLocalGmail();
             const response1 = await fetch(`http://localhost:5000/student/get-info`, {
                 method: 'POST',
                 headers: {
@@ -63,12 +36,11 @@ function Result() {
                 body: JSON.stringify({
                     username: username,
                 }),
-            });
-
+            }); 
             if (response1['status'] === 200) {
-                const data1 = await response1.json();
+                const data1 = response1.json();
                 console.log(data1.studentId);
-                const response = await fetch('http://localhost:5000/student/get-class-list', {
+                fetch('http://localhost:5000/student/get-class-list', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -77,36 +49,59 @@ function Result() {
                     body: JSON.stringify({
                         studentId: data1.studentId,
                     }),
-                });
-                const data = await response.json();
-                setRow(
-                    data.map((obj, index) => ({
-                       
-                        term: obj.classId,
-                       
-                    })),
-                );
-                console.log(data);
+                })
+                    // .then((response) => response.json())
+                    // .then((data) =>
+                    //     this.setState(() => ({
+                    //         dataAPI: data.map((obj) => obj),
+                    //         rows: data.map((obj, index) => ({
+                    //             id: index + 1,
+                    //             SubID: obj?.subjectId,
+                    //             SubName: obj?.subjectName,
+                    //             Midterm: obj?.midterm,
+                    //             credit: obj?.term,
+                    //             Finalterm: obj?.final,
+                    //             //locationName: obj?.locationName,
+                                
+                    //         })),
+                    //     })),
+                    // )
+                    // .catch((error) => {
+                    //     console.error('Error:', error);
+                    // });
             }
-        }
-        fetchMyAPI();
-    }, []);
+            
+    }
 
-    
-    return (
-        <Container maxWidth="md" sx={{ mt: 5, mb: 5 }}>
-            <Box sx={{ height: 'auto', width: '100%' }}>
-                <DataGrid
-                    sx={{ p: 2 }}
-                    rows={rows}
-                    columns={columns}
-                    disableSelectionOnClick
-                    rowsPerPageOptions={['ALL']}
-                    autoHeight
-                />
-            </Box>
-        </Container>
-    );
+    render() {
+        return (
+            <Container maxWidth="lg">
+                <Container maxWidth="lg" sx={{ mt: 5, mb: 10 }}>
+                    <div style={{ height: 630, width: '94%' }}>
+                        <DataGrid
+                            disableSelectionOnClick
+                            rows={this.state.rows}
+                            columns={this.state.columns}
+                            pageSize={10}
+                            rowsPerPageOptions={[10]}
+                        />
+                        {/* <div id="details-form" />
+                        <SubjectDialog
+                            savechange={this.saveChange}
+                            Notify={this.Notify}
+                            count={this.state.rows.length}
+                        />
+                    </div> */}
+                    </div>
+                </Container>
+                <Snackbar open={this.state.open} autoHideDuration={6000} onClose={this.handleClose}>
+                    <Alert onClose={this.handleClose} severity={this.state.severity} sx={{ width: '100%' }}>
+                        {this.state.message}
+                    </Alert>
+                </Snackbar>
+            </Container>
+        );
+    }
 }
 
 export default Result;
